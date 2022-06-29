5a3fbb46162ae87f9860e3abb478b07a06114e32
gabrielvaz081 <vazgabriel081@gmail.com>
2022-06-28 22:35:34 -0300

    Opção cadastrar adicionada
    Opção cadastrar adicionada

--- login.py
+++ login.py
@@ -1,9 +1,5 @@
-
-
-
-
 from PyQt5 import uic, QtWidgets
-
+import sqlite3
 
 def validar():
     Login.lbl_erro.setText("")
@@ -33,6 +29,22 @@ def cadastrar():
 
     # Fazer a condicional para cadastrar no banco de dados
 
+    if (senha == repetir_senha):
+        try:
+            conexao_banco = sqlite3.connect('dados_clientes.db')
+            cursor = conexao_banco.cursor()
+            cursor.execute('CREATE TABLE IF NOT EXISTS dados(nome text, login text, senha text')
+            cursor.execute('INSERT INTO dados Values ('"+nome+"', '"+login+"', '"+senha+"')')
+
+            conexao_banco.commit()
+            conexao_banco.close()
+            Login.lbl_erro.setText("Usuário cadastrado com sucesso!")
+
+        except sqlite3.Error as erro:
+            print('Erro na inserção de dados: ', erro)
+
+    else:
+        Login.lbl_erro.setText("As senhas digitadas são distintas!")
 
 
 app = QtWidgets.QApplication([])
@@ -43,6 +55,7 @@ Cadastro = uic.loadUi("Cadastro.ui")
 
 Login.btn_entrar.clicked.connect(validar)
 Login.btn_cadastro.clicked.connect(exibe_cadastro)
+Cadastro.btn_cadastro.clicked.connect(cadastrar)
 Tela03.pushButton_2.clicked.connect(logout)
 Login.line_senha.setEchoMode(QtWidgets.QLineEdit.Password)
 
